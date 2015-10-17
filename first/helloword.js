a78fec45a54f47609e98d7641d8316d212953ab8
smliepoker <smliepoker@users.noreply.github.com>
2015-10-17 12:11:36 +0800

    Create helloword.js
    开始node.js的远航

---
+++ first/helloword.js
@@ -0,0 +1,11 @@
+/*
+  这是简单的在控制台打印hello world
+  在这之前 首先你的电脑要安装node.js 
+  具体过程，不再赘述，百度获去官网，很容易做到
+*/
+
+
+console.log('hello world');
+/*
+  打开 git.bash (当然要安装) ，之后cd到helloword.js 所在文件夹 输入node helloword.js 就会有效果出现啦 ^_^
+*/
